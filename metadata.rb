2dc0090ff53a8245c61ed785226676ebd7bca6ed
Thirumoorthi R <moorthy978@gmail.com>
2017-09-08 02:39:10 +0530

    Initial commit

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,6 @@
 name 'motd_all'
-maintainer 'The Authors'
-maintainer_email 'you@example.com'
+maintainer 'ThiruMoorthi'
+maintainer_email 'moorthy978@gmail.com'
 license 'all_rights'
 description 'Installs/Configures motd_all'
 long_description 'Installs/Configures motd_all'
@@ -8,8 +8,8 @@ version '0.1.0'
 
 # If you upload to Supermarket you should set this so your cookbook
 # gets a `View Issues` link
-# issues_url 'https://github.com/<insert_org_here>/motd_all/issues' if respond_to?(:issues_url)
+issues_url 'https://github.com/thirumoorthir/motd_all/issues' if respond_to?(:issues_url)
 
 # If you upload to Supermarket you should set this so your cookbook
 # gets a `View Source` link
-# source_url 'https://github.com/<insert_org_here>/motd_all' if respond_to?(:source_url)
+source_url 'https://github.com/thirumoorthir/motd_all' if respond_to?(:source_url)
