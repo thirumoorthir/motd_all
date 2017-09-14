7665ab64c28f648a5c91a21993b87a35a2e59dff
Thirumoorthi R <thirumoorthir@users.noreply.github.com>
2017-09-15 01:59:47 +0530

    adding some code

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,18 +1,11 @@
-#
-# Cookbook Name:: motd_all
-# Recipe:: default
-#
-# Copyright (c) 2017 The Authors, All Rights Reserved.
 # vim: syntax=ruby:expandtab:shiftwidth=2:softtabstop=2:tabstop=2
+# Copyright (c) 2016-present, Facebook, Inc.
+# All rights reserved.
 #
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree. An additional grant
 # of patent rights can be found in the PATENTS file in the same directory.
 #
-
-template '/etc/motd' do
-  group 'root'
-  mode '0644'
-  owner 'root'
-  source 'motd.erb'
-end
+default['fb_motd'] = {
+  'extra_lines' => [],
+}
